c7cbd21e2f7c48b583e2b8f2c163aa36e147c544
Kent14042084 <e.moskvyak@gmail.com>
2022-01-11 10:15:30 +0200

    task 1-20

---
+++ src/temp.java
@@ -0,0 +1,42 @@
+import java.util.Date;
+import java.util.Scanner;
+public class temp {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        /*
+        Date date = new Date();
+
+        System.out.println("введите текущее количество часов ");
+        int hours = sc.nextInt();
+        System.out.println("введите текущее количество минут ");
+        int minutes = sc.nextInt();
+        if (date.toString().contains("Dec 30"))
+        {
+
+            int current_minutes = ((24-hours)*60)+(60-minutes);
+            System.out.println("до нового года осталось " + (current_minutes + 1440) + " минут");
+        }
+        else if (date.toString().contains("Dec 31"))
+        {
+            int current_minutes = ((24-hours)*60)+(60-minutes);
+            System.out.println("до нового года осталось " + (current_minutes) + " минут");
+        }
+        else
+        {
+            System.out.println("До нового года больше чем два дня");
+        }
+        */
+        /*
+        Напишите класс, который принимает с клавиатуры число (двузначное)
+        и выводит на экран сумму первой и второй цифры.
+        */
+        System.out.println("enter Integer value 1");
+        int number = sc.nextInt();
+        int first_value = (a + b) / 10;
+        System.out.println(first_value);
+        int second_value = first_value %10;
+        System.out.println(second_value);
+
+
+    }
+}
